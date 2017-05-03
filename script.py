974c83db2e8e940241d5469a76b6a460294ab330
LingCheng3273 <lcheng3@stuy.edu>
2017-05-03 19:12:56 -0400

    done!

--- script.py
+++ script.py
@@ -26,18 +26,23 @@ def run(filename):
     tmp = []
     step = 0.1
     for command in commands:
-        print command
         if command[0] == "push":
             print "pushing"
             tmp= stack[len(stack)-1]
             stack.append(tmp)
+            
         elif command[0] == "pop":
+            print "popping"
             stack.pop()
+            
         elif command[0] == "move":
+            print "moving"
             tmp= make_translate(command[1], command[2], command[3])
             matrix_mult(stack[len(stack)-1], tmp)
             stack[len(stack)-1] = tmp
+            
         elif command[0] == "rotate":
+            print "rotating"
             theta = float(command[2]) * (math.pi / 180)
 
             if command[1] == 'x':
@@ -46,10 +51,68 @@ def run(filename):
                 t = make_rotY(theta)
             else:
                 t = make_rotZ(theta)
-            matrix_mult(stack[len(stack)-1], t)
-            stack[len(stack)-1] = t
-        elif command[0] == 'scale':
-            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+                
             matrix_mult(stack[len(stack)-1], t)
             stack[len(stack)-1] = t
             
+        elif command[0] == "scale":
+            print "scaling"
+            t = make_scale(float(command[1]), float(command[2]), float(command[3]))
+            matrix_mult(stack[len(stack)-1], t)
+            stack[len(stack)-1] = t
+            
+        elif command[0] == "box":
+            print "box"
+            tmp= []
+            add_box(tmp,
+                    float(command[1]), float(command[2]), float(command[3]),
+                    float(command[4]), float(command[5]), float(command[6]))
+            matrix_mult(stack[len(stack)-1], tmp)
+            draw_polygons(tmp, screen, color)
+            tmp = []
+                    
+        elif command[0] == "sphere":
+            print "sphere"        
+            tmp= []
+            add_sphere(tmp,
+                       float(command[1]), float(command[2]), float(command[3]),
+                       float(command[4]), step)
+            matrix_mult(stack[len(stack)-1], tmp)
+            draw_polygons(tmp, screen, color)
+            tmp = []
+            
+        elif command[0] == 'torus':
+            print "torus"
+            tmp= []
+            add_torus(tmp,
+                      float(command[1]), float(command[2]), float(command[3]),
+                      float(command[4]), float(command[5]), step)
+            matrix_mult(stack[len(stack)-1], tmp)
+            draw_polygons(tmp, screen, color)
+            tmp = []
+
+        elif command[0] == "line":
+            print "line"
+            tmp= []
+            add_edge(tmp,
+                     float(command[0]), float(command[1]), float(command[2]),
+                     float(command[3]), float(command[4]), float(command[5]) )
+            matrix_mult(stack[len(stack)-1], tmp)
+            draw_lines(tmp, screen, color)
+
+        elif command[0] == "save":
+            save_extention(screen, command[1])
+
+        elif command[0] == "display":
+            display(screen)
+            
+
+                    
+
+
+
+
+
+
+
+
